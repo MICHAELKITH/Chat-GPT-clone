c58b7019e0b34ae8e250b12e2fd78e05ceb00195
MICHAELKITH <michaelkithinji27@gmail.com>
2023-08-31 22:14:47 +0300

    Add assets

--- gpt-clone/src/App.js
+++ gpt-clone/src/App.js
@@ -5,8 +5,13 @@ function App() {
     <div className="App">
       <div className="sidebar">
         <div className="upperSideTop">
-          <img />
-
+          <img src="" alt="" className="logo" />
+          <span className="btand">Chat Gtp</span>
+          <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat +</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt="" />How To Join Microverse</button>
+            <button className="query"><img src="" alt="" />How To Join Microverse</button>
+          </div>
         </div>
         <div className="upperside">
 
@@ -14,8 +19,7 @@ function App() {
         <div className="lowerside">
 
         </div>
-        <div className='main'>
-        
+        <div className='main'>     
       </div>
       </div>
     </div>
