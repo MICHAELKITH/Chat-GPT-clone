fe7ba2cdcf10bc00745c40735c0eb921506f21d6
MICHAELKITH <michaelkithinji27@gmail.com>
2023-09-18 22:42:03 +0300

    Add new images

--- gpt-clone/src/App.js
+++ gpt-clone/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
-
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
 function App() {
   return (
     <div className="App">
@@ -11,20 +12,20 @@ function App() {
           <span className="brand">Black CodeX</span>
           </div>
           <button className="midBtn">
-            <img src="" alt="" className="addBtn" />
+            <img src={addBtn} alt="" className="addBtn" />
             New Chat
           </button>
           <div className="upperSideBottom">
             <button className="query">
-              <img src="" alt="" />
+              <img src={msgIcon} alt="" />
               What is AI?
             </button>
             <button className="query">
-              <img src="" alt="" />
+              <img src={msgIcon} alt="" />
               How to connect Ruby On Rails with React?
             </button>
             <button className="query">
-              <img src="" alt="" />
+              <img src={msgIcon} alt="" />
               What is ORM?
             </button>
           </div>
