b0cb0c3fbb369aeb560c0aa15b8e669526fe30cc
MICHAELKITH <michaelkithinji27@gmail.com>
2023-09-26 22:07:23 +0300

    Add .env file for privacy

--- gpt-clone/src/openai.js
+++ gpt-clone/src/openai.js
@@ -1,33 +1,39 @@
-// const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
+const apiKey = process.env.Api_key;
+const conf = new Configuration({ apiKey: apiKey });
+const openai = new OpenAIApi(conf);
 
-// const conf = new Configuration({ apiKey: "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" });
-// const openai = new OpenAIApi(conf);
-
-// export async function sendMsgToApi(message) {
-//   const res = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: message, // Corrected from "promt"
-//     temperature: 0.7,
-//     maxTokens: 256, // Corrected from "max_token"
-//     topP: 1, // Corrected from "top_p"
-//     frequencyPenalty: 0, // Corrected from "frequency_penalty"
-//     presencePenalty: 0, // Corrected from "presence_penalty"
-//   });
-
-//   return res.data.choices[0].text;
-// }
-
-
-const OpenAI = require("openai");
-const openai = new OpenAI({
-  apiKey: ""
-});
-const openFun=async()=>{
-const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": "YOUR PROMPT TEXT",}],
-    max_tokens:30
+export async function sendMsgToApi(message) {
+  const res = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message, // Corrected from "promt"
+    temperature: 0.7,
+    max_tokens: 256, // Corrected from "maxTokens"
+    top_p: 1, // Corrected from "topP"
+    frequency_penalty: 0, // Corrected from "frequencyPenalty"
+    presence_penalty: 0, // Corrected from "presencePenalty"
   });
-  console.log(chatCompletion.choices[0].message.content);
+
+  return res.data.choices[0].text;
 }
-openFun();
+
+
+
+// const OpenAI = require("openai");
+
+// const apiKey = process.env.Api_key; // Make sure you have the environment variable Api_key set with your API key.
+
+// const openai = new OpenAI({
+//   apiKey: apiKey
+// });
+
+// const openFun = async () => {
+//   const chatCompletion = await openai.chat.completions.create({
+//     model: "gpt-3.5-turbo",
+//     messages: [{ "role": "user", "content": "YOUR PROMPT TEXT" }],
+//     max_tokens: 30
+//   });
+//   console.log(chatCompletion.choices[0].message.content);
+// };
+
+// openFun();
